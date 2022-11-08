6b24f76be58c38c65efec61f02bdffef1bdd693c
Mahmood <taswermahmood@gmail.com>
2022-11-07 21:49:28 -0500

    Update Skills.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,14 +4,14 @@
             <div data-aos-duration="1500" data-aos="fade-left" style="padding-bottom:2%">
                 <div style="font-family: 'Lilita One', cursive; font-size: 40px; padding-bottom: 1%;"> Skills</div>
                 <div style="font-weight:bold; padding:1% 0%">Technical Skills</div>
-                <div> <strong> Engineering Software: </strong> Autodesk REVIT, ETABS, AutoCAD, Geo5, Geotechnical Graphics (Quicklog), SAP 2000.</div>
-                <div> <strong> Image Processing Software: </strong> Adobe Photoshop, Adobe Illustrator.</div>
-                <div> <strong> Programming: </strong> C++.</div>
+                <li> <strong> Engineering Software: </strong> Autodesk REVIT, ETABS, AutoCAD, Geo5, Geotechnical Graphics (Quicklog), SAP 2000.</li>
+                <li> <strong> Image Processing Software: </strong> Adobe Photoshop, Adobe Illustrator.</li>
+                <li> <strong> Programming: </strong> C++.</li>
                 <div style="font-weight:bold; padding-top:1%">Lab Skills</div>
-                <div>
+                <li>
                     Universal Testing Machine (UTM), Triaxial Test, Compaction Test, Consolidation Test,
                     Permeability Test, Direct Shear Test, Index properties tests etc.
-                </div>
+                </li>
                 <v-row justify="center">
                     <v-col cols="2"><v-img class="white--text align-end " height="260px" src="../assets/lab1.jpg"></v-img></v-col>
                     <v-col cols="2"><v-img class="white--text align-end " height="260px" src="../assets/lab2.jpg"></v-img></v-col>
